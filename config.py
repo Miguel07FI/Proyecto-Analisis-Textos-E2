89a1986789f0d5a5d72f64befc511ba9a77924ff
Miguel07FI <miguelhernandez0532@gmail.com>
2026-06-03 22:16:21 -0600

    Ampliacion de BD

--- config.py
+++ config.py
@@ -26,32 +26,33 @@ EMBEDDING_DIM = 64
 BATCH_SIZE = 64            
 EPOCHS = 20                
 
-# Lexico especializado compilado mediante patrones morfologicos (Regex)
+
 LEXICON_INSULTOS = [
-    # Cadenas base con flexiones gramaticales basicas
+    # --- Núcleos de Insultos e Improperios Directos ---
     r"pendej[oa]s?", r"estupid[oa]s?", r"estúpid[oa]s?", r"imbécil(es)?", r"imbecil(es)?",
-    r"idiotas?", r"mierdas?", r"put[oa]s?", r"cabrón(es)?", r"cabron[aa]s?",
-    r"inept[oa]s?", r"basuras?", r"escorias?", r"infeli(z|ces)", r"lacras?", 
-    r"parásitos?", r"parasitos?", r"ignorantes?", r"miserables?", r"ratas?", 
-    r"incompetentes?", r"maldit[oa]s?", r"culer[oa]s?", r"perr[oa]s?",
+    r"idiotas?", r"mierdas?", r"put[oa]s?", r"cabrón(es)?", r"cabron[aa]s?", r"culer[oa]s?",
+    r"inept[oa]s?", r"basuras?", r"escorias?", r"infeli(z|ces)", r"lacras?", r"perr[oa]s?",
+    r"parásitos?", r"parasitos?", r"ignorantes?", r"miserables?", r"ratas?", r"malparid[oa]s?",
+    r"incompetentes?", r"maldit[oa]s?", r"zoquetes?", r"tarad[oa]s?", r"babos[oa]s?",
+    r"pendejadas?", r"pendejetes?", r"huevón(es)?", r"huevona(s)?", r"pinches?", r"pencos?",
+    r"mediocres?", r"fracasad[oa]s?", r"nefast[oa]s?", r"estorbos?", r"escorias?",
 
-    # Patrones orientados a deshumanizacion y adjetivos hostiles
-    r"asqueros[oa]s?", r"repulsiv[oa]s?", r"ascos?", r"vómitos?", r"vómitos?",
-    r"plagas?", r"parásitos?", r"parásitas?", r"garrapatas?", r"piojos[oa]s?",
-    r"marranos?", r"puerc[oa]s?", r"cerd[oa]s?", r"animal(es)?", r"bestias?",
-    r"retrógradas?", r"mediocres?", r"fracasad[oa]s?", r"nefast[oa]s?",
+    # --- Adjetivos de Deshumanización, Desprecio y Crueldad ---
+    r"asqueros[oa]s?", r"repulsiv[oa]s?", r"ascos?", r"vómitos?", r"plagas?", r"parásitas?",
+    r"garrapatas?", r"piojos[oa]s?", r"marranos?", r"puerc[oa]s?", r"cerd[oa]s?", r"animal(es)?",
+    r"bestias?", r"retrógradas?", r"mugros[oa]s?", r"resentid[oa]s?", r"manos[oa]s?", r"chillón(es)?",
+    r"chillon[aa]s?", r"mentiros[oa]s?", r"sinvergüenzas?", r"sinverguenzas?", r"usurer[oa]s?",
 
-    # Localismos regionales y argot informal de redes sociales
-    r"chingad[oa]s?", r"chingaquedito", r"cul[oa]s?", r"maricones?", r"maricón",
-    r"jotos?", r"nacos?", r"nacas?", r"mamones?", r"mamonas?", r"mamón",
-    r"babos[oa]s?", r"tarad[oa]s?", r"zoquetes?", r"pendejadas?", r"pendejetes?",
-    r"huevón(es)?", r"huevona(s)?", r"pinches?", r"pencos?", r"mediocres?",
+    # --- Argot de Redes, Discriminación e Insultos Regionales ---
+    r"maricones?", r"maricón", r"jotos?", r"nacos?", r"nacas?", r"mamones?", r"mamonas?", r"mamón",
+    r"lamebotas", r"arrastrad[oa]s?", r"vendid[oa]s?", r"chayoter[oa]s?", r"lacras?", r"ratas?",
+    r"rater[oa]s?", r"corrupt[oa]s?", r"mafios[oa]s?", r"vándalos?", r"vándalas?", r"delincuentes?",
+    r"criminal(es)?", r"asesin[oa]s?", r"malnacid[oa]s?", r"desgraciad[oa]s?",
 
-    # Indicadores de violencia directa, imperativos y hostilidad dirigida
-    r"lacras?", r"ratas?", r"rater[oa]s?", r"corrupt[oa]s?", r"mafios[oa]s?",
-    r"vándalos?", r"vándalas?", r"delincuentes?", r"criminal(es)?", r"asesin[oa]s?",
-    r"malnacid[oa]s?", r"desgraciad[oa]s?", r"maldit[oa]s?", r"linchar?", r"muérete",
-    r"muéranse", r"mátanlo", r"mátenlos", r"lárgate", r"lárguense", r"estorbos?"
+    # --- Verbos de Acción Violenta e Imperativos Directos (Máxima Alerta) ---
+    r"linchar?", r"muérete", r"muéranse", r"mátanlo", r"mátenlos", r"lárgate", r"lárguense", 
+    r"quemar?", r"colgar?", r"destruir?", r"eliminar?", r"desaparecer?", r"partirle", 
+    r"madrear?", r"chingar?", r"revienten", r"golpear?", r"funar?"
 ]
 
 def obtener_regex_lexicon():
--- descargar_data.py
+++ descargar_data.py
@@ -3,118 +3,138 @@ import pandas as pd
 import random
 from config import DATASET_CSV
 
-print("\n[CLI] Generando conjunto de datos masivo expandido para corregir falsos positivos...")
+print("\n[SADOW BACKEND] Generando super-dataset balanceado de alta sensibilidad (30,000 instancias)...")
 
-# 1. Ampliación de la Clase 1: Discurso de Odio (Matrices Léxicas Multi-Estructura)
-sujetos_odio = [
-    "Ese usuario", "El creador", "Este tipo", "Esa mujer", "El streamer", "El candidato", 
-    "Este ratero", "Ese sinvergüenza", "Pinche perro", "Mugroso resentido", "Esa lacra", 
-    "El político", "Los trabajadores", "Ese mentiroso", "Usurero", "Esa basura", "Gente ignorante"
+# --- BANCO DE DATOS EXHAUSTIVO PARA LA CLASE 1 (DISCURSO DE ODIO / HOSTILIDAD) ---
+sujetos_hostiles = [
+    "Ese usuario", "El creador del video", "Este tipo ratero", "Esa mujer", "El streamer mentiroso",
+    "El candidato corrupto", "Ese sinvergüenza", "Pinche perro chillón", "Mugroso resentido ignorante",
+    "Esa lacra mafiosa", "El político delincuente", "Los usureros de las empresas", "Esa basura humana",
+    "Gente retrógrada", "Este delincuente", "El moderador inepto", "Esa lacra social", "Ese infeliz"
 ]
 
-verbos_odio = [
-    "es una completa", "demuestra ser una", "se comporta como una", "no es más que una", 
-    "resultó ser una", "insulta a todos como", "roba y es una", "da asco ver que es una"
+verbos_hostiles = [
+    "es una completa", "demuestra ser una", "se comporta como una", "no es más que una",
+    "resultó ser una", "insulta a la sociedad como", "roba a los ciudadanos siendo una",
+    "da un asco profundo porque es una", "esconde que es una", "promueve ser una"
 ]
 
-insultos_odio = [
-    "escoria", "lacra", "basura", "rata", "parásito", "plaga", "mierda", "incompetente", 
+nucleos_odio = [
+    "escoria", "lacra", "basura", "rata", "parásito", "plaga", "mierda", "incompetente",
     "miserable", "inepto", "ignorante", "pendejo", "pendeja", "imbécil", "idiota", "cabrón",
-    "ratero", "sinvergüenza", "chillón", "mentiroso", "manoso", "corrupto", "mediocre"
+    "ratero", "sinvergüenza", "chillón", "mentiroso", "manoso", "corrupto", "mediocre", "puercos",
+    "marrano", "bestia nefasta", "fracasado", "lacra social", "vándalo", "criminal"
 ]
 
-conclusiones_odio = [
-    "deberían banearlo ya", "hay que lincharlo entre todos", "da asco que exista", 
-    "lárgate de esta plataforma", "ojalá te pase algo malo", "eres un peligro social",
-    "hay que quemar todo", "nadie le cree nada", "debería desaparecer ya"
+conclusiones_violentas = [
+    "deberían banearlo ya de internet", "hay que lincharlo entre todos", "da asco que exista en la plataforma",
+    "lárgate de esta comunidad", "ojalá te pase algo malo y te mueras", "eres un peligro social estorbando",
+    "hay que quemar todo y destruirlos", "nadie le cree por chillón", "debería desaparecer de inmediato",
+    "hay que partirle la madre", "no compren nada con ese usurero", "mátenlo por rata"
 ]
 
-# Frases crudas completas inspiradas en comentarios reales de internet (Ataques dirigidos)
-frases_odio_directas = [
-    "Quien es este tipo ratero singuenza mugroso tipo resentido ignorante y manoso.",
-    "A este perro mentiroso nadie le cree por chillón.",
-    "Usurero ay que quemar Elektra no comprar nada.",
-    "Malditos corruptos de mierda ojalá se mueran todos.",
-    "Qué pinche asco de persona, una verdadera basura humana.",
-    "Lárguense de aquí malditas plagas nadie los quiere.",
-    "Este delincuente debería estar en la cárcel, escoria social."
+# Corpus de validación empírica basado en tus capturas reales de error
+comentarios_odio_reales = [
+    "Quien es este tipo ratero Con que moral insulta à la presidenta si el es un ratero sinverguenxa que a sus trabajadores los have trabajar 7 dias à la semana y con un horario de 12 horas y dias festivos Mugroso tipo resentido ignorante y manoso",
+    "A este perro mentiroso nadie le cree por chillón",
+    "Usurero ay que quemar Elektra no comprar nada",
+    "Malditos corruptos ojalá se mueran todos",
+    "Pinche viejo ratero muérete ya",
+    "Hay que linchar a ese infeliz escoria de la sociedad",
+    "Lárguense de aquí malditas plagas asquerosas",
+    "Este canal es una puta mierda y el creador es un pendejo incompetente",
+    "Qué maldito asco me da este tipo, ojalá lo desaparezcan",
+    "Puras mentiras de este cerdo mentiroso desgraciado"
 ]
 
-# 2. Ampliación de la Clase 0: Mensajes Neutros, Saludos y Control Coloquial
-saludos_y_agradecimientos = [
-    "Hola Wicha te mando saludos y muchas Gracias por tus Noticias.",
-    "Excelente video amigo muchas gracias por compartir la información.",
-    "Saludos cordiales profesor desde la Facultad de Ingeniería.",
-    "Hola buenos días gracias por el gran aporte al canal.",
-    "Hola qué buen contenido me suscribo de inmediato saludos.",
-    "Muchas gracias por mantenernos informados todos los días.",
-    "Hola comunidad espero que tengan una excelente semana bendiciones."
+# --- BANCO DE DATOS EXHAUSTIVO PARA LA CLASE 0 (CONTROL / NO ODIO / CASUAL) ---
+saludos_neutrales = [
+    "Hola Wicha te mando saludos y muchas Gracias por tus Noticias",
+    "Excelente video amigo muchas gracias por compartir la información y mantener el canal activo",
+    "Saludos cordiales profesor desde la Facultad de Ingeniería de la UNAM",
+    "Hola buenos días excelente participación gracias por el gran aporte al canal",
+    "Hola qué buen contenido me suscribo de inmediato saludos desde México",
+    "Muchas gracias por mantenernos informados con objetividad todos los días",
+    "Hola comunidad espero que tengan una excelente semana llena de éxito, bendiciones",
+    "Buenas tardes saludos al equipo técnico por la excelente transmisión en vivo",
+    "Gracias por el análisis detallado, un fuerte abrazo para todo el equipo",
+    "Hola un gusto saludarte de nuevo, sigo tus videos desde hace años"
 ]
 
-vulgari_casual = [
-    "Qué pendejo estoy, olvidé el archivo en la otra computadora.",
-    "El examen de la facultad estuvo de la chingada, pero alcancé a pasar.",
-    "No mames, está cabrón el tráfico de hoy en la avenida principal.",
-    "Qué pendejada acabo de hacer con el café, lo derramé sobre el teclado.",
-    "Ya valió madre, se cayó el sistema de la escuela otra vez.",
-    "Está cabrón terminar este proyecto final para el lunes.",
-    "Puta madre me dolió mucho el golpe en el pie.",
-    "Qué buena peda nos pusimos ayer estuvo chingón."
+vulgari_mexicano_casual = [
+    "Qué pendejo estoy, olvidé el archivo de la materia en la otra computadora.",
+    "El examen de la facultad estuvo de la chingada, pero alcancé a pasar de milagro.",
+    "No mames, está cabrón el tráfico de hoy en la avenida principal para llegar a clases.",
+    "Qué pendejada acabo de hacer con el café, lo derramé completo sobre el teclado.",
+    "Ya valió madre, se cayó el sistema de inscripciones de la escuela otra vez.",
+    "Está cabrón terminar este proyecto final de programación para el lunes.",
+    "Puta madre me dolió mucho el golpe que me di en el pie con la mesa.",
+    "Qué buena peda nos pusimos ayer con los amigos estuvo chingón todo.",
+    "Ese examen de cálculo estuvo bien pinche difícil la verdad.",
+    "No mames wey, casi me atropella un carro cruzando la avenida."
 ]
 
-neutros_academia = [
-    "El proyecto final de ingeniería quedó agendada para el próximo lunes.",
-    "Recomiendo ampliamente este libro para aprender estructuras de datos.",
-    "Muchas gracias por compartir el enlace con la documentación de la API.",
-    "¿Alguien sabe a qué hora abre la biblioteca central de la facultad?",
-    "Excelente participación de los alumnos en la conferencia de tecnología.",
-    "El procesamiento de lenguaje natural requiere un análisis estadístico profundo."
+textos_academicos_puros = [
+    "El proyecto final de ingeniería quedó agendada para el próximo lunes por la tarde.",
+    "Recomiendo ampliamente este libro para aprender estructuras de datos y algoritmos complejos.",
+    "Muchas gracias por compartir el enlace con la documentación de la API de Google.",
+    "¿Alguien sabe a qué hora abre la biblioteca central de la facultad de ingeniería?",
+    "Excelente participación de los alumnos en la conferencia magistral de tecnología avanzada.",
+    "El procesamiento de lenguaje natural moderno requiere un análisis estadístico profundo.",
+    "Las redes neuronales convolucionales ofrecen una excelente ventaja en análisis de señales.",
+    "El algoritmo de optimización lineal resuelve problemas de asignación de recursos de manera óptima.",
+    "La configuración del servidor requiere permisos de administrador del sistema de red.",
+    "Se convoca a los estudiantes al seminario de inteligencia artificial aplicada de este semestre."
 ]
 
-# Inicialización de semilla estática para garantizar la reproducibilidad matemática
+# Inicialización del entorno computacional balanceado
 random.seed(42)
 registros = []
-total_registros = 20000  # Aumentamos a 20,000 ejemplos para darle más estabilidad a la CNN
+total_registros = 30000  # Escalado masivo para máxima generalización de la CNN 1D
 mitad_registros = total_registros // 2
 
-# --- GENERACIÓN DE LA CLASE 1: DISCURSO DE ODIO ---
+# --- GENERACIÓN DE LA CLASE 1: DISCURSO DE ODIO (15,000 ejemplos) ---
 while len([r for r in registros if r["label"] == 1]) < mitad_registros:
     prob = random.random()
     if prob < 0.6:
-        # Combinatoria clásica dinámica
-        txt = f"{random.choice(sujetos_odio)} {random.choice(verbos_odio)} {random.choice(insultos_odio)} y {random.choice(conclusiones_odio)}."
-    elif prob < 0.9:
-        # Estructuras explícitas e implícitas variadas
-        txt = f"{random.choice(sujetos_odio)} debería desaparecer, arruina la comunidad y {random.choice(conclusiones_odio)}."
+        # Combinatoria masiva sintáctica
+        txt = f"{random.choice(sujetos_hostiles)} {random.choice(verbos_hostiles)} {random.choice(nucleos_odio)} y {random.choice(conclusiones_violentas)}"
+    elif prob < 0.85:
+        # Variantes de amenazas directas
+        txt = f"{random.choice(sujetos_hostiles)} es una {random.choice(nucleos_odio)}, {random.choice(conclusiones_violentas)}"
     else:
-        # Inyección directa de las frases del mundo real con las que fallaba
-        txt = random.choice(frases_odio_directas)
-    
-    # Ruido estructural por capitalización
+        # Inyección directa de patrones del mundo real para erradicar los falsos negativos
+        txt = random.choice(comentarios_odio_reales)
+        
+    # Inyección de ruido estructural tipográfico (Mayúsculas)
     if random.random() > 0.8: 
         txt = txt.upper()
         
     registros.append({"texto": txt, "label": 1})
 
-# --- GENERACIÓN DE LA CLASE 0: NO ODIO / CONTROL ---
+# --- GENERACIÓN DE LA CLASE 0: NO ODIO / CONTROL (15,000 ejemplos) ---
 while len([r for r in registros if r["label"] == 0]) < mitad_registros:
     prob = random.random()
     if prob < 0.4:
-        txt = random.choice(saludos_y_agradecimientos)
+        # Inyección masiva de saludos para blindar el modelo contra falsos positivos
+        txt = random.choice(saludos_neutrales)
+        # Añadir ligeras variaciones aleatorias para que no memorice el texto exacto
+        if random.random() > 0.5:
+            txt = f"{txt} !!!"
     elif prob < 0.7:
-        txt = random.choice(vulgari_casual)
+        txt = random.choice(vulgari_mexicano_casual)
     else:
-        txt = random.choice(neutros_academia)
-    
-    # Concatenación de interjecciones coloquiales peyorativas sin blanco dirigido (ruido estructural)
-    if random.random() > 0.7 and txt not in saludos_y_agradecimientos:
+        txt = random.choice(textos_academicos_puros)
+        
+    # Concatenación de interjecciones ruidosas peyorativas no dirigidas
+    if random.random() > 0.7 and txt not in saludos_neutrales:
         txt = f"{txt} Qué dolor de cabeza, puta madre."
         
     registros.append({"texto": txt, "label": 0})
 
-# Consolidación, aleatorización e instanciacion del DataFrame final
+# Consolidación, aleatorización y almacenamiento físico
 df_masivo = pd.DataFrame(registros)
 df_masivo = df_masivo.sample(frac=1, random_state=42).reset_index(drop=True)
 df_masivo.to_csv(DATASET_CSV, index=False, encoding='utf-8')
 
-print(f"REGISTRO: ¡Dataset corregido con éxito! Exportado a '{DATASET_CSV}' ({total_registros} instancias balanceadas).")
+print(f"REGISTRO: Conjunto de datos ultra-sensible exportado exitosamente a '{DATASET_CSV}' con {total_registros} instancias binarias.")
